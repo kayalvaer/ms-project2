d6e9222b77fc7e91da7e584c50d5fe5c85adc465
Kay Alvaer <62592167+Kayalvaer@users.noreply.github.com>
2020-11-27 00:34:39 +0100

    test diagonal combinations

--- javascript/script.js
+++ javascript/script.js
@@ -7,6 +7,7 @@ let player2 = 2
 let currentPlayer = player1 //var to check curr player
 let pick = false
 let won = 0 //0 means ongoing game, 1 means a win, 2 is a draw
+let started = false //check if game is started
 
 //check console for array
 console.log(playBoard)
@@ -16,6 +17,12 @@ function nodeClick(position) {
     //if not picking we placing
     if (pick) {
         playBoard[position] = emptySpace
+
+        //set source to empty, to remove token
+        let img = "#node-" + (position + 1) + " img"
+        $(img).attr("src", "")
+
+
         pick = false
         if (currentPlayer == player1) {
             currentPlayer = player2
@@ -42,7 +49,7 @@ function nodeClick(position) {
     console.log(playBoard)
 
     //check current player won
-    if (calcMatch()) {
+    if (calcMatch(position)) {
         pick = true
         sendAlerts()
         return; // stop function from continuing
@@ -58,18 +65,45 @@ function nodeClick(position) {
     //write send alerts
     sendAlerts()
 }
-//calculate matches
-let fakeWin = 0
 
-//assuming 5th click is a match
-function calcMatch() {
-    fakeWin++
-    if (fakeWin % 5 == 0) {
-        return true
-    } else {
-        return false
+//Calculate the win
+function calcMatch(position) {
+    if ([1, 4, 7, 16, 19, 22, 13, 10].includes(position)) {
+        if (playBoard[position + 1] == currentPlayer && playBoard[position - 1] == currentPlayer) {
+            return true
+        }
     }
+    //corner combinations -1,-2 test
+    if ([2, 5, 8, 11, 14, 17, 20, 23].includes(position)) {
+        if (playBoard[position - 2] == currentPlayer && playBoard[position - 1] == currentPlayer) {
+            return true
+        }
+    }
+    //matching diagonals
+    if ([2, 5, 8].includes(position)) {
+        if (playBoard[2] == currentPlayer && playBoard[5] == currentPlayer && playBoard[8] == currentPlayer) {
+            return true
+        }
+    }
+
+    if ([17, 20, 23].includes(position)) {
+        if (playBoard[17] == currentPlayer && playBoard[20] == currentPlayer && playBoard[23] == currentPlayer) {
+            return true
+        }
+    }
+    if ([15, 18, 21].includes(position)) {
+        if (playBoard[15] == currentPlayer && playBoard[18] == currentPlayer && playBoard[21] == currentPlayer) {
+            return true
+        }
+    }
+    if ([0, 3, 6].includes(position)) {
+        if (playBoard[0] == currentPlayer && playBoard[3] == currentPlayer && playBoard[6] == currentPlayer) {
+            return true
+        }
+    }
+
 }
+
 
 function sendAlerts() {
     // string concatenation to glue strings together
@@ -99,6 +133,7 @@ function startGame() {
     currentPlayer = player1
     pick = false //currently not on pick mode
     won = 0 //track if game has entered won state
+    started = true //starts the game
 
     sendAlerts()
 }
@@ -108,7 +143,8 @@ $(document).ready(function () {
     $(".node").click(function () {
         //replacing node- with to make it a number and subtracting 1 to make it start from 0
         let node = this.id.replace("node-", "") - 1
-        nodeClick(node)
+        if (started) // clicking of node only start if game is started
+            nodeClick(node)
 
     })
 
